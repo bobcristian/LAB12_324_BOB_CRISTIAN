e2b59b84a7ad27c60be0fa6abc336c03650a0e89
bobcristian <bobcristian96@yahoo.com>
2016-12-19 08:33:15 +0200

    final

--- 1.cpp
+++ 1.cpp
@@ -7,8 +7,8 @@ int main()
 {
     Stack<int> myIntStack;
     Stack<float> myFloatStack;
-    int nr=1;
-    myIntStack.push(nr);
+
+    myIntStack.push(20);
     myIntStack.push(4);
     myFloatStack.push(20.5);
     myFloatStack.push(3.45);
