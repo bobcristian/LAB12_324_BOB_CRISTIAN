d103f5441a47fb6d448266436c4b66ab6272c444
bobcristian <bobcristian96@yahoo.com>
2016-12-19 08:28:46 +0200

    initial

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+    Stack<int> myIntStack;
+    Stack<float> myFloatStack;
+    int nr=1;
+    myIntStack.push(nr);
+    myIntStack.push(4);
+    myFloatStack.push(20.5);
+    myFloatStack.push(3.45);
+
+    myIntStack.print();
+    cout << endl;
+    myFloatStack.print();
+    cout << endl;
+    myFloatStack.pop();
+    myFloatStack.print();
+
+    return 0;
+}
